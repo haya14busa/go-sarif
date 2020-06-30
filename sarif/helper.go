037a9a3f07539d8bfa6a7437bb43a1b4541f8443
haya14busa <haya14busa@gmail.com>
2020-06-30 12:10:06 +0000

    Define Ptr() helpers for other enum as well

--- sarif/helper.go
+++ sarif/helper.go
@@ -17,6 +17,16 @@ func Int64(v int64) *int64       { return &v }
 func Float64(v float64) *float64 { return &v }
 func String(v string) *string    { return &v }
 
-// Ptr is a helper to get a pointer to Level type as we cannot get a reference
+// Ptr are helpers to get a pointer to enum value as we cannot get a reference
 // to defined const values.
-func (l Level) Ptr() *Level { return &l }
+
+func (v Role) Ptr() *Role                       { return &v }
+func (v Level) Ptr() *Level                     { return &v }
+func (v Content) Ptr() *Content                 { return &v }
+func (v BaselineState) Ptr() *BaselineState     { return &v }
+func (v Importance) Ptr() *Importance           { return &v }
+func (v ResultKind) Ptr() *ResultKind           { return &v }
+func (v SuppressionKind) Ptr() *SuppressionKind { return &v }
+func (v State) Ptr() *State                     { return &v }
+func (v Version) Ptr() *Version                 { return &v }
+func (v ColumnKind) Ptr() *ColumnKind           { return &v }
